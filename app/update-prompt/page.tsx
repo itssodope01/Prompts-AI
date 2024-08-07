4fa542a35559474813238060e169f3db3b6e950b
Pritam Chakraborty <pritam.amit29@gmail.com>
2024-08-07 03:59:41 +0200

    Update Readme e.4.5f

--- app/update-prompt/page.tsx
+++ app/update-prompt/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Suspense } from "react";
 import EditPrompt from "./EditPrompt";
 
@@ -10,3 +11,83 @@ const EditPromptPage = () => {
 };
 
 export default EditPromptPage;
+=======
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import Form from "@/components/Form";
+
+const EditPrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const searchParams = useSearchParams();
+  const promptID = searchParams.get("id");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchPrompt = async () => {
+      try {
+        const res = await fetch(`/api/prompt/${promptID}`);
+        const data = await res.json();
+        setPost({ prompt: data.prompt, tag: data.tag });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (promptID) {
+      fetchPrompt();
+    }
+  }, [promptID]);
+
+  const updatePrompt = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!promptID) return alert("Prompt ID is missing");
+
+    setSubmitting(true);
+
+    try {
+      const res = await fetch(`/api/prompt/${promptID}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (res.ok) {
+        setPost({
+          prompt: "",
+          tag: "",
+        });
+        router.push("/profile");
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
+  );
+};
+
+export default EditPrompt;
+>>>>>>> 279bf28 (Initial Commit)
